e47b627553412513f9150de93044dda186c7834a
yycquant0812 <xherobruce@gmail.com>
2026-03-17 10:45:22 +0800

    解題流程

---
+++ d068該減肥了.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int weight;
+	cin >> weight;
+	int condition;
+	condition = (weight > 50); 
+	cout << (weight - condition) << endl;
+
+	return 0;
+}
