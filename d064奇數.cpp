291185e7a5a125b137313968964b284481cb3261
yycquant0812 <xherobruce@gmail.com>
2026-03-17 11:15:33 +0800

    Add program to check if a number is odd or even

---
+++ d064奇數.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i;
+	cin >> i;
+
+	if ((i % 2) == 1) {
+		cout << "Odd" << endl;
+	}else {
+		cout << "Even" << endl;
+	}
+	return 0;
+}
